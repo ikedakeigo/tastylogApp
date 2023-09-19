f5f20a845b354a9b626d7706e2d0bec8d3f2e76c
silverskg <chokichoki_gm@i.softbank.jp>
2023-09-19 19:17:21 +0900

    expressの導入

--- app.js
+++ app.js
@@ -1,2 +1,11 @@
 const PORT = process.env.PORT;
-console.log(`Application listening at ${PORT}`);
+const express = require("express");
+const app = express();
+
+app.set("view engine", "ejs");
+
+app.use("/", require("./routes/index.js"));
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
